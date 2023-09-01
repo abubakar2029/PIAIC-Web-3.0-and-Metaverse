e8bf911bb5f136483f6e3de31d83d0716083102f
Muhammad AbuBakar <abubakarzafar2029@gmail.com>
2023-09-01 16:26:42 +0500

    Added content of Typescript

---
+++ Typescript/Content/Tuples.ts
@@ -0,0 +1,6 @@
+/* Tuples have fix order and length */
+// let tupleArr1: [string, number]=[23,"Hello"] // giving error bcz of wrong order
+let tupleArr2: [string, number] = ["Hello", 23]
+// let tupleArr3: [string, number] = ["Hello"] // giving error bcz 2nd value is missing 
+let tupleArr4: [string, number?] = ["Hello", 23]
+// ? => made 2nd value optional
---
+++ Typescript/Content/Union.ts
@@ -0,0 +1,6 @@
+
+
+let unionArr1: (string | number)[] = ["Hello", 2023];
+let unionArr2: (string | number)[] = [2023, "Hello"];
+//both are correct => this will contain only numbers and strings
+let unionArr3: (string | boolean)[] = [true, "Hello"];
